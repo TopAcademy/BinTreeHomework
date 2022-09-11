a0d149cac33ace75141cc9267c4ad04d0309e3e3
Maxim Huzmiev <max@huzm.ru>
2022-09-11 13:59:50 +0300

    New method tested on sample tree

--- BinTree1.cpp
+++ BinTree1.cpp
@@ -10,7 +10,18 @@ int main()
 {
 	BinaryTree tree(3);
 	tree.add_node(1);
+	tree.add_node(2);
+	tree.add_node(45);
 	tree.add_node(8);
+	tree.add_node(0);
+	tree.add_node(7);
+	tree.add_node(12);
+	tree.add_node(9);
+	tree.add_node(5);
+	tree.add_node(6);
+	// Show tree
 	tree.show_no_rec();
+	cout << endl;
+	tree.show_tree();
 
 }
