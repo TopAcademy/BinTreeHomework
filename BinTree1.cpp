2c894c84e86643f69ff8a8986416f7c973f0aa0d
Max Huzmiev <max@huzm.ru>
2022-09-12 20:24:57 +0300

    Try to use string nodes

--- BinTree1.cpp
+++ BinTree1.cpp
@@ -9,29 +9,12 @@ using std::string;
 
 int main()
 {
-	BinaryTree<int> tree(3);
-	tree << 1;
-	tree << 2;
-	tree << 8;
-	tree << 0;
-	tree << 7;
-	tree << 12;
-	tree << 45;
-	tree << 9;
-	tree << 5;
-	tree << 6;
-	/*
-	tree.add_node(1);
-	tree.add_node(2);
-	tree.add_node(8);
-	tree.add_node(0);
-	tree.add_node(7);
-	tree.add_node(12);
-	tree.add_node(45);
-	tree.add_node(9);
-	tree.add_node(5);
-	tree.add_node(6);
-	*/
+	BinaryTree<string> tree("Nicolas");
+	tree << "Biden";
+	tree << "Swarzenegger";
+	tree << "Obama";
+	tree << "Trump";
+	tree << "Putin";
 	// Show tree
 	tree.show_no_rec();
 	cout << endl;
